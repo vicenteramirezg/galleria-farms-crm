4b625c7efca30a48928ce8af814c713b211890ba
vramirezgalleria <vramirez@galleriafarms.com>
2025-03-06 12:14:31 -0500

    Fix CRM app import path in INSTALLED_APPS

--- floral_crm/floral_crm/settings.py
+++ floral_crm/floral_crm/settings.py
@@ -41,7 +41,7 @@ ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 INSTALLED_APPS = [
     'django.contrib.humanize',
-    'floral_crm.crm',
+    'crm',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
