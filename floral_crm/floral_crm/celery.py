30fa5621bc52424aeda6e9b67432fd81b2601e1e
vicenteramirezg <vicente.ramirezg@gmail.com>
2025-03-11 11:50:51 -0400

    Trying to fix celery.

--- floral_crm/floral_crm/celery.py
+++ floral_crm/floral_crm/celery.py
@@ -1,17 +1,19 @@
+from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-# Set default Django settings module for Celery
+# Set the default Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'floral_crm.settings')
 
+# Create the Celery app
 app = Celery('floral_crm')
 
-# Load task modules from all registered Django apps
+# Load settings from Django settings.py
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Auto-discover tasks in installed apps
-app.autodiscover_tasks(lambda: [n.name for n in app.conf.installed_apps])
+# Auto-discover tasks from all registered Django apps
+app.autodiscover_tasks()
 
-@app.task(bind=True)
+@app.task(bind=True, ignore_result=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
