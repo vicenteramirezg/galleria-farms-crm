a3b959b731d1f2dc3182ada5b085a4db23c34ee4
vicenteramirezg <vicente.ramirezg@gmail.com>
2025-03-12 14:26:18 -0400

    Final fix I hope.

--- floral_crm/floral_crm/settings.py
+++ floral_crm/floral_crm/settings.py
@@ -184,14 +184,9 @@ if DEBUG:
     import mimetypes
     mimetypes.add_type("image/x-icon", ".ico", True)
 
-# Celery settings
-if IS_PRODUCTION:
-    CELERY_BROKER_URL = os.getenv('REDIS_URL')
-else:
-    CELERY_BROKER_URL = 'redis://localhost:6379/0'
-
+CELERY_BROKER_URL = os.getenv('REDIS_URL')
 CELERY_TASK_ALWAYS_EAGER = False
-CELERY_RESULT_BACKEND = os.getenv("REDIS_URL")  # ✅ Use Redis as the backend too
+CELERY_RESULT_BACKEND = os.getenv("REDIS_URL")
 CELERY_RESULT_EXTENDED = True
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
